ce226a2557ec17bf0ffa1391c50d676ae70e56cf
jarangit <jaran.dch@gmail.com>
2021-12-09 23:29:50 +0700

    add menu

--- components/layout/menu.js
+++ components/layout/menu.js
@@ -1,11 +1,68 @@
-import React from 'react'
-
+import {
+  Button,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+} from "@mui/material";
+import React from "react";
+import styled from "styled-components";
+import LanguageIcon from "@mui/icons-material/Language";
+const Div = styled.div`
+  background-color: #f9f9f9;
+  padding: 0 60px;
+  height: 100px;
+`;
 const Menu = () => {
-    return (
-        <div>
-            this menu
-        </div>
-    )
-}
+  return (
+    <Div>
+      <Grid
+        container
+        spacing={2}
+        textAlign="center"
+        alignItems="center"
+        sx={{ height: "80px" }}
+      >
+        <Grid item xs={12} sm={3}>
+          <Stack
+            spacing={2}
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <img
+              src="https://cdn.zeplin.io/5e6c97d09536901139b8706b/assets/59B582B6-D023-406B-8BE0-F90FE96E2CCB.png"
+              width="80"
+            />
+            <a href="/">Rent</a>
+            <a href="/">Buy</a>
+            <a href="/">Projects</a>
+          </Stack>
+        </Grid>
+        <Grid item xs={12} sm={5}></Grid>
+        <Grid item xs={12} sm={4}>
+          <Stack direction="row" justifyContent="space-between">
+            <a href="/">Guide</a>
+            <a href="/">Agent partner</a>
+            <a href="/">Eng</a>
+            {/* <List>
+              <ListItem>
+                <ListItemText primary="Eng" />
+                <ListItemIcon>
+                  <LanguageIcon />
+                </ListItemIcon>
+              </ListItem>
+            </List> */}
+            <Button variant="contained" style={{background: "#65acf0"}}>
+              Sign in
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Div>
+  );
+};
 
-export default Menu
+export default Menu;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import { Button, createTheme, ThemeProvider, Typography } from "@mui/material";
 
+const theme = createTheme({
+  typography: {
+    fontSize: 12,
+    h1: 32,
+    h2: 24,
+    h3: 18,
+    h4: 14,
+    body1: 16,
+  },
+  palette: {
+    primary: {
+      main: "#002161",
+      light: "#757ce8",
+      dark: '#002884',
+
+    },
+  },
+});
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+        <Button color="primary" variant="contained">
+          Hi
+        </Button>
+        <Typography variant="h1">Jaran</Typography>
+      </Layout>
+    </ThemeProvider>
   );
 }
 
