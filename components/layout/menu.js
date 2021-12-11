43b3bd73248a7f8d00691525ec5d9f42039f41c6
jarangit <jaran.dch@gmail.com>
2021-12-11 14:10:06 +0700

    add dropdown menu

--- components/layout/menu.js
+++ components/layout/menu.js
@@ -6,14 +6,56 @@ import {
   ListItemIcon,
   ListItemText,
   Stack,
+  Typography,
 } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
 import LanguageIcon from "@mui/icons-material/Language";
+
 const Div = styled.div`
   background-color: #f9f9f9;
   padding: 0 60px;
   height: 100px;
+
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: white;
+    border-radius: 5px;
+    box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.2);
+    z-index: 2;
+    margin-top: 12px;
+    ul {
+      text-align: left;
+      padding: 0;
+      h4 {
+        margin-left: 20px;
+      }
+      li {
+        list-style-type: none;
+        margin: 10px 0;
+        a {
+          padding: 0 20px;
+        }
+        a:hover {
+          background-color: whitesmoke;
+        }
+      }
+    }
+  }
+
+  .dropdown:hover .dropdown-content {
+    display: block;
+  }
+  .but_blue {
+    padding: 8px 15px;
+    border-radius: 5px;
+  }
+  .but_blue:hover {
+    background-color: #65acf0;
+    transition: 0.51s;
+    color: white;
+  }
 `;
 const Menu = () => {
   return (
@@ -36,7 +78,29 @@ const Menu = () => {
               src="https://cdn.zeplin.io/5e6c97d09536901139b8706b/assets/59B582B6-D023-406B-8BE0-F90FE96E2CCB.png"
               width="80"
             />
-            <a href="/">Rent</a>
+            <div className="dropdown">
+              <a className="but_blue" href="/">
+                Rent
+              </a>
+              <div className="dropdown-content">
+                <ul>
+                  <Typography variant="h4">Bangkok, Thailand</Typography>
+                  <li>
+                    <a href="#">House for rent in Bangkok</a>
+                  </li>
+                  <li>
+                    <a href="#">House for rent in Bangkok</a>
+                  </li>
+                  <li>
+                    <a href="#">House for rent in Bangkok</a>
+                  </li>
+                  <li>
+                    <a href="#">House for rent in Bangkok</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
             <a href="/">Buy</a>
             <a href="/">Projects</a>
           </Stack>
@@ -55,7 +119,7 @@ const Menu = () => {
                 </ListItemIcon>
               </ListItem>
             </List> */}
-            <Button variant="contained" style={{background: "#65acf0"}}>
+            <Button variant="contained" style={{ background: "#65acf0" }}>
               Sign in
             </Button>
           </Stack>
