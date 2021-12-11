9935e631b875ace0341d6d4f5a51a4474c7b7ab5
jarangit <jaran.dch@gmail.com>
2021-12-11 13:20:10 +0700

    complete html footer

---
+++ components/layout/copy_right.js
@@ -0,0 +1,25 @@
+import { Stack } from "@mui/material";
+import React from "react";
+import styled from 'styled-components'
+
+
+const Copy_right = () => {
+  return (
+    <div>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <div>
+          <a>Logo</a>
+          <a>Contact us</a>
+          <a>Career</a>
+          <a>Policy</a>
+          <a>Policy</a>
+        </div>
+        <div>
+          <p>Copyright © 2021 Accom Group. All rights reserved.</p>
+        </div>
+      </Stack>
+    </div>
+  );
+};
+
+export default Copy_right;
