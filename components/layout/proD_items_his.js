34c3b76815809de4d19994c55d456533e5550bcc
jarangit <jaran.dch@gmail.com>
2021-12-20 15:05:59 +0700

    zoom image when hover prod history

--- components/layout/proD_items_his.js
+++ components/layout/proD_items_his.js
@@ -1,11 +1,23 @@
 import React, { Fragment } from "react";
 import { Grid } from "@mui/material";
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Div_image  = styled.div`
+const Div_image = styled.div`
   width: 100%;
-  
-`
+  overflow: hidden;
+  border-radius: 10px;
+  margin: 2px 0;
+  transition: transform .2s; /* Animation */
+  :hover {
+    overflow: hidden;
+
+    .image_thumb {
+      transform: scale(1.1);
+      transition: transform .2s; /* Animation */
+
+    }
+  }
+`;
 const ProD_items_his = () => {
   const fake_data = [
     {
@@ -42,12 +54,13 @@ const ProD_items_his = () => {
       {fake_data.map((items, key) => (
         <Grid container spacing={2} key={key}>
           <Grid item xs={5} sm={5}>
-            <div>
+            <Div_image>
               <img
                 src="https://cdn.zeplin.io/5e6c97d09536901139b8706b/assets/CCC24B5E-39B2-49B4-826C-0FF410C11756.png"
                 width="100%"
+                className="image_thumb"
               />
-            </div>
+            </Div_image>
           </Grid>
           <Grid item xs={7} sm={7}>
             <p>Langsuan ville</p>
