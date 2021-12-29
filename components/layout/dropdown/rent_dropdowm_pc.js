a66869916267371ff6d964cde11a7b87095ec5d5
jarangit <jaran.dch@gmail.com>
2021-12-30 03:26:19 +0700

    Fix When Checked Radio Have to secon click

--- components/layout/dropdown/rent_dropdowm_pc.js
+++ components/layout/dropdown/rent_dropdowm_pc.js
@@ -1,13 +1,9 @@
 import React, { useState, useContext } from "react";
-import {
-  FormControlLabel,
-  FormControl,
-  RadioGroup,
-} from "@mui/material";
+import { FormControlLabel, FormControl, RadioGroup } from "@mui/material";
 import Radio from "@mui/material/Radio";
 import { makeStyles } from "@mui/styles";
 import { AuthContext } from "../../../appState/authProviceder";
-
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 const useStyles = makeStyles({
   fontSize: {
@@ -20,17 +16,16 @@ const useStyles = makeStyles({
 const Rent_dropdowm_pc = (props) => {
   const classes = useStyles();
 
-  const { data_check_rent, setdata_check_rent,} = useContext(AuthContext);
-
+  const { data_check_rent, setdata_check_rent } = useContext(AuthContext);
 
   const [selected, setselected] = useState(false);
   const [getValueSelected, setgetValueSelected] = useState("");
 
-  function onSelect(e) {
+  const onSelect = async (e) => {
+    await setdata_check_rent(e.target.value);
     setgetValueSelected(e.target.value);
-    setdata_check_rent(e.target.value)
-    console.log(e.target.value);
-  }
+    console.log(getValueSelected);
+  };
 
   return (
     <div className="dropdown-content">
@@ -44,7 +39,13 @@ const Rent_dropdowm_pc = (props) => {
                   className={`jr_hover_blue ${classes.fontSize}`}
                   key={key}
                   value={sub_items}
-                  control={<Radio name={sub_items} sx={{ fontSize: "19px" }} />}
+                  control={
+                    <Radio
+                      name={sub_items}
+                      checked={sub_items === data_check_rent ? true : false}
+                      sx={{ fontSize: "19px" }}
+                    />
+                  }
                   label={sub_items}
                   name={sub_items}
                   onChange={onSelect}
