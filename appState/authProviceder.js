d4bc83ba61eb613cfb40d43ffb9fc525516db7a3
jarangit <jaran.dch@gmail.com>
2022-01-07 20:23:44 +0700

    Mian megrge develop

--- appState/authProviceder.js
+++ appState/authProviceder.js
@@ -13,13 +13,8 @@ const AuthProvider = ({ children, userData }) => {
   const [data_check_rent, setdata_check_rent] = useState("Rent");
   const [onCheckedContactForm, setonCheckedContactForm] = useState(false);
   const [onConpleteContactForm, setonConpleteContactForm] = useState(false);
-<<<<<<< HEAD
-  const [gobal_filter_menu_api, setgobal_filter_menu_api] = useState([]);
-  const [Gobal_search_filter_api, setGobal_search_filter_api] = useState([]);
-=======
   const [Selected_bads, setSelected_bads] = useState("")
   const [ArrPopertyChecked, setArrPopertyChecked] = useState([])
->>>>>>> main
   return (
     <AuthContext.Provider
       value={{
@@ -33,18 +28,9 @@ const AuthProvider = ({ children, userData }) => {
         setonCheckedContactForm,
         onConpleteContactForm,
         setonConpleteContactForm,
-<<<<<<< HEAD
-        data_check_rent,
-        setdata_check_rent,
-        gobal_filter_menu_api,
-        setgobal_filter_menu_api,
-        Gobal_search_filter_api,
-        setGobal_search_filter_api,
-=======
         data_check_rent, setdata_check_rent,
         Selected_bads, setSelected_bads,
         ArrPopertyChecked, setArrPopertyChecked,
->>>>>>> main
       }}
     >
       {children}
--- components/layout/fillter/search_input.js
+++ components/layout/fillter/search_input.js
@@ -2,22 +2,14 @@ import React, { useState, useContext, useEffect } from "react";
 import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 import styled from "styled-components";
-<<<<<<< HEAD
-import { AuthContext } from "../../../appState/authProviceder";
-import ShowResultProject from "./search/show_result_project";
-import Show_result_location from "./search/show_result_location";
-import Show_result_Train from "./search/show_result_train";
-import Show_result_school from "./search/show_result_school";
-=======
 import { Typography } from "@mui/material";
 import Transportation_svg from "../../../lib/img/svg/transportation";
 import Location_svg from "../../../lib/img/svg/location";
 import Buildding_svg from "../../../lib/img/svg/buildding";
 import School_svg from "../../../lib/img/svg/school";
-
->>>>>>> main
+import { AuthContext } from "../../../appState/authProviceder";
 const Div = styled.div`
-  .jr_icon_inside_input {
+  .jr_icon_inside_input 
     position: absolute;
     right: 65px;
     top: 0;
@@ -167,9 +159,6 @@ const Search_input = () => {
       </form>
       {dataSearch !== "" ? (
         <div className="dropdown_search">
-<<<<<<< HEAD
-          <ShowResult data={DataFecth} />
-=======
           <div>
             <strong className="underline_text jr_f16">Location</strong>
             <a
@@ -233,7 +222,6 @@ const Search_input = () => {
               </div>
             </a>
           </div>
->>>>>>> main
         </div>
       ) : (
         ""
