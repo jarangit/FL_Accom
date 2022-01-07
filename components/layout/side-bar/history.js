064d7cdf1abc6770b594ae86d95571533ec1abeb
jarangit <jaran.dch@gmail.com>
2022-01-07 21:39:08 +0700

    add api his

--- components/layout/side-bar/history.js
+++ components/layout/side-bar/history.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProD_items_his from "./proD_items_his";
 
 const History = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [dataFetch, setdataFetch] = useState([]);
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://www.accomasia.co.th/api/v1/history")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setdataFetch(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [1]);
+  // console.log(isLoaded);
+  // console.log(dataFetch);
   return (
     <div>
       <p
@@ -10,8 +34,13 @@ const History = () => {
       >
         My history
       </p>
-
-      <ProD_items_his/>
+      {isLoaded ? (
+        <ProD_items_his
+          data={dataFetch.data}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
--- components/layout/side-bar/proD_items_his.js
+++ components/layout/side-bar/proD_items_his.js
@@ -18,7 +18,8 @@ const Div_image = styled.div`
     }
   }
 `;
-const ProD_items_his = () => {
+const ProD_items_his = (props) => {
+  console.log(props);
   const fake_data = [
     {
       image:
@@ -51,33 +52,40 @@ const ProD_items_his = () => {
   ];
   return (
     <>
-      {fake_data.map((items, key) => (
-        <a href="#" key={key}>
-          <Grid container spacing={2}>
-            <Grid item xs={5} sm={5}>
-              <Div_image>
-                <Image
-                  src="https://i.ibb.co/1RDJj8Y/1.png"
-                  width={180}
-                  height={140}
+      {props.data ? (
+        <>
+          {props.data.hotproperty.map((items, key) => (
+            <a href = {items.link} key={key}>
+              <Grid container spacing={2}>
+                <Grid item xs={5} sm={5}>
+                  <Div_image>
+                    {/* <Image
+                      src="https://i.ibb.co/1RDJj8Y/1.png"
+                      width={180}
+                      height={140}
+                      className="image_thumb"
+                    /> */}
+                    <img
+                  src={items.photo}
+                  // width="180"
+                  height="140"
                   className="image_thumb"
                 />
-                {/* <img
-                  src="https://cdn.zeplin.io/5e6c97d09536901139b8706b/assets/CCC24B5E-39B2-49B4-826C-0FF410C11756.png"
-                  width="135"
-                  className="image_thumb"
-                /> */}
-              </Div_image>
-            </Grid>
-            <Grid item xs={7} sm={7}>
-              <p className="jr_f14">Langsuan ville</p>
-              <p className="jr_f14">491.96 SQ.M. | 3 beds | 3+1 baths</p>
-              <p className="jr_f14 jr_blod">for sale</p>
-              <p className="text_color_sky jr__f16 jr_blod">฿ 77,000,000</p>
-            </Grid>
-          </Grid>
-        </a>
-      ))}
+                  </Div_image>
+                </Grid>
+                <Grid item xs={7} sm={7}>
+                  <p className="jr_f14">{items.property_name}</p>
+                  <p className="jr_f14">{`${items.landvalue} | ${items.bedroom} beds | ${items.bathroom} baths`}</p>
+                  <p className="jr_f14 jr_blod">{items.purpose}</p>
+                  <p className="text_color_sky jr__f16 jr_blod">{`฿ ${items.rent_price}`}</p>
+                </Grid>
+              </Grid>
+            </a>
+          ))}
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 };
