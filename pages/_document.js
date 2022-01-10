a8be563229b3a598fef090562c437c5b63e567e1
jarangit <jaran.dch@gmail.com>
2022-01-10 23:22:00 +0700

    Test add font

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-        <link href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" rel="stylesheet" />
-
+          <link
+            rel="preload"
+            href="../styles/font/Montserrat-Regular.ttf"
+            as="font"
+            crossOrigin=""
+          />
         </Head>
         <body>
           <Main />
